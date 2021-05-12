88fc3d9fb217373a663be9e7186679353b62a021
cdk18 <https://github.com/caodakai/test2104.git>
2021-05-12 10:59:39 +0800

    第二次提交

--- 01-test/src/Demo1.java
+++ 01-test/src/Demo1.java
@@ -1,5 +1,5 @@
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println("''''''''''''''''''''''''''''''''''''''''");
+        System.out.println("https://github.com/caodakai/test2104.git");
     }
 }
