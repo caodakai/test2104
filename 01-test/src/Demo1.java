439681baaeebaebaea3f705f450bba5440f5b488
cdk18 <https://github.com/caodakai/test2104.git>
2021-05-12 11:25:59 +0800

    第二次提交

--- 01-test/src/Demo1.java
+++ 01-test/src/Demo1.java
@@ -1,5 +1,8 @@
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("https://github.com/caodakai/test2104.git");
+
+
+        System.out.println("吴华飞是沙雕，建议红烧");
     }
 }
